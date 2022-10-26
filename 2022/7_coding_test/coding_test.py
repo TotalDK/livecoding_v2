b0a98ac987bd148a9ec167adad10516d313c7678
Simon <simon009@breum.dk>
2022-10-26 09:50:09 +0200

    Added descriptive hints to coding_test.py

--- 2022/7_coding_test/coding_test.py
+++ 2022/7_coding_test/coding_test.py
@@ -1,1 +1,21 @@
-#Will be uploaded after the Coding Re-test
+# Write a function called specialmean that takes a positive integer n as input. Given this integer input, 
+# calculate the mean value of the following list: [1,2,...,n,2**1,2**2,...,2**n].
+
+# For example, specialmean(5) should return the mean value of the list [1,2,3,4,5,2,4,8,16,32], which is 7.7.
+# Hint: To calculate the mean of a list, feel free to use Python's mean() function by adding the following line before your 
+# function definition:  from statistics import mean
+
+# If the input is not a positive integer, the function should return 0. 
+# Hint: isinstance(n,int) returns True if n is an integer.
+
+from statistics import mean
+
+
+
+def specialmean(n):
+	#Actual code will be uploaded after the Coding Re-test
+	return
+
+def read_csv(filename):
+	#Actual code will be uploaded after the Coding Re-test
+	return
